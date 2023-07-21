df3c4d30177f4368bc8cac2c3c38d45c43508708
Zven94 <nicobolanez@gmail.com>
2023-07-21 00:09:28 -0300

    Add function to add book into the API

--- src/redux/books/bookSlice.jsx
+++ src/redux/books/bookSlice.jsx
@@ -1,51 +1,118 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  booksArr: [{
-    item_id: Math.floor(Math.random() * (9999 - 1000)),
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-  },
-  {
-    item_id: Math.floor(Math.random() * (9999 - 1000)),
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-  },
-  {
-    item_id: Math.floor(Math.random() * (9999 - 1000)),
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-  }],
+  booksArr: [],
+  status: 'not started',
+  error: null
 };
+
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps';
+const apiKey = 'dQhdHp7wgEct1umI7942';
+
+// ask for books datat
+export const fetchBooks = createAsyncThunk(
+  'books/fetchBooks',
+  async () => {
+    try {
+      const response = await axios.get(`${baseURL}/${apiKey}/books`);
+      const { data } = response;
+      const booksData = Object.keys(data).map((key) => ({
+        item_id: key,
+        ...data[key][0]
+      }));
+      return booksData;
+    } catch (error) {
+      return Promise.reject(error.message ? error.message : error);
+    }
+  },
+);
+
+// send new book to API
+export const addBook = createAsyncThunk(
+  'books/addNewBook',
+  async (newBook) => {
+    try {
+      const bookData = {
+        ...newBook,
+        item_id: Math.floor(Math.random() * (9999 - 1000)),
+        category: 'Science fiction',
+      };
+      const response = await axios.post(`${baseURL}/${apiKey}/books`, bookData);
+      return response.data;
+    } catch (err) {
+      return err.message;
+    }
+  },
+);
+
+// delete books from API
+export const removeBook = createAsyncThunk(
+  'books/deleteBook',
+  async (bookID) => {
+    try {
+      const response = await axios.delete(`${baseURL}/${apiKey}/books/${bookID}`);
+      return response.data;
+    } catch (err) {
+      return err.message;
+    }
+  },
+);
 
 // Includes an array of books
 const bookSlice = createSlice({
   name: 'books',
   initialState,
-  reducers: {
-    addBook: (state, action) => {
-      const newBook = {
-        item_id: Math.floor(Math.random() * (9999 - 1000)),
-        ...action.payload,
-      };
-
-      const isDuplicate = state.booksArr.some((book) => book.title === newBook.title
-        && book.author === newBook.author);
-
-      if (!isDuplicate) {
-        state.booksArr.push(newBook);
+  extraReducers(builder) {
+    builder
+      // loading action
+      .addCase(fetchBooks.pending, (state) => ({
+        ...state,
+        status: 'loading',
+      }))
+      // action has been succeeded
+      .addCase(fetchBooks.fulfilled, (state, action) => ({
+        ...state,
+        status: 'succeeded',
+        books: action.payload.map((book) => ({
+          ...book,
+          currentChapter: 'Chapter 1',
+          completed: '0%',
+        })),
+      }))
+      // the action was cancel or there were an error
+      .addCase(fetchBooks.rejected, (state, action) => ({
+        ...state,
+        status: 'failed',
+        error: action.error.message,
+      }))
+      // the action has created a new book into the list
+      .addCase(addBook.fulfilled, (state, action) => {
+       // console.log('imin')
+        if (action.payload === 'created') {
+          return {
+            ...state,
+            status: 'succeeded',
+          };
+        }
         return state;
-      }
-      return state;
-    },
-    removeBook: (state, action) => ({
-      ...state,
-      booksArr: state.booksArr.filter((book) => book.item_id !== action.payload),
-    }),
-  },
+      })
+      // the action has deleted a new book into the list
+      .addCase(removeBook.fulfilled, (state, action) => {
+        if (action.payload === 'The book was deleted successfully!') {
+          return {
+            ...state,
+            status: 'succeeded',
+          };
+        }
+        return state;
+      });
+  }
 });
-export const { addBook, removeBook } = bookSlice.actions;
+
+//export const { addBook, removeBook } = bookSlice.actions;
+export const AllBooks = (state) => state.books.books;
+export const getStatus = (state) => state.books.status;
+export const getError = (state) => state.books.error;
+
 export default bookSlice.reducer;
--- src/routers/components/addingBook.jsx
+++ src/routers/components/addingBook.jsx
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../../redux/books/bookSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchBooks, removeBook, AllBooks } from '../../redux/books/bookSlice';
 
 const ReloadList = ({ bookData }) => {
   const dispatch = useDispatch();
+  const books = useSelector(AllBooks);
 
   const handleRemove = () => {
     dispatch(removeBook(bookData.item_id));
   };
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch, books]);
 
   return (
     <li key={bookData.item_id}>
